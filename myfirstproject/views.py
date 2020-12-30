31be53616513cf0550a092c2814ee415a111573d
Nikolai <Nikolai@MacBook-Pro.local>
2020-12-30 09:30:49 +0300

    frist commit

---
+++ myfirstproject/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def about(request):
+	return HttpResponse('this is about page')
+	
+
+def home(request):
+	return render(request, 'home.html', {'greeting':'Hello!'})
+	
